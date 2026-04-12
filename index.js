962961ad4b4a5878595e37290cb8ed67c6560735
plevf <porleventeferenc@gmail.com>
2026-04-12 19:21:05 +0200

    done

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const button = document.querySelector("button");
-button.disabled = true;
-const table = document.querySelector("table");
+const tbody = document.querySelector("tbody")
 
-fetch("https://api.siposm.hu/getDevelopers") // then: olyan mint az await, tehat akkor fut le amikor kesz
-    .then((r) => r.json())
-    .then((developers) => {
+fetch("https://api.siposm.hu/getDevelopers")
+.then(r => r.json())
+.then(developers => {
+    for (const developer of developers) {
+        const tr = document.createElement("tr")
 
-        for (const developer of developers) {
-            const tr = document.createElement("tr")
-            const td_name = document.createElement("td")
-            td_name.append(developer.name)
+        const name_td = document.createElement("td")
+        name_td.append(developer.name)
 
-            const td_email = document.createElement("td")
-            td_email.append(developer.email)
-
-            const td_age = document.createElement("td")
-            td_age.append(developer.age)
-
-            const td_salary = document.createElement("td")
-            td_salary.append(developer.salary)
-
-            tr.append(td_name, td_email, td_age, td_salary)
-            table.append(tr)
+        const email_td = document.createElement("td")
+        email_td.append(developer.email)
+        
+        const age_td = document.createElement("td")
+        if(developer.age>37){
+            age_td.classList.add("old")
         }
+        age_td.append(developer.age)
 
-        button.disabled = false; // ha betoltodott az api hivas akkor lesz elerheto a gomb
-    });
+        const salary_td = document.createElement("td")
+        salary_td.append(developer.salary)
 
+        tr.append(name_td, email_td, age_td, salary_td)
+        tbody.appendChild(tr)
+    }
+    const lowest = lowestEarning(developers)
+    console.log(lowest)
+})
+
+function lowestEarning(developers){
+    if(!developers?.length){ // ha length pl 3 akkor az true, ezert tagadjuk
+        return null
+    }
+
+    let min = developers[0];
+
+    for (let index = 1; index < developers.length; index++) {
+        const developer = developers[index];
+        if(developer.salary<min.salary){
+            min = developer
+        }
+    }
+    return min
+}
